5cf65986d4b176ffeec8732585e963becb977e12
Lexi Ross <the.circ@gmail.com>
2012-07-29 09:47:44 -0400

    typo

--- scripts/load.js
+++ scripts/load.js
@@ -6,7 +6,7 @@ $(document).bind('pageinit', function() {
             checkins.push('<li class="ui-li ui-li-static ui-body-c"><p class="ui-li-aside ui-li-desc">'
                             + checkin.timestamp
                             + '</p><h3 class="ui-li-heading">'
-                            + "Conquest #" + checkin.usee_id
+                            + "Conquest #" + checkin.useeid
                             + '</h3><p class="ui-li-desc">'
                             + checkin.pts
                             + ' points</p></li>');
