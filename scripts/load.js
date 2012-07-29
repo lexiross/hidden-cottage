5b801c963490f37e68bfc0713f0af439d5093c82
Lexi Ross <the.circ@gmail.com>
2012-07-29 08:43:23 -0400

    syntax

--- scripts/load.js
+++ scripts/load.js
@@ -5,9 +5,9 @@ $(document).bind('pageinit', function() {
         $.each(data, function(index, checkin) {
             checkins.push('<li class="ui-li ui-li-static ui-body-c"><p class="ui-li-aside ui-li-desc">'
                             + checkin.timestamp
-                            + '</p><h3 class="ui-li-heading">
+                            + '</p><h3 class="ui-li-heading">'
                             + checkin.name
-                            + '</h3><p class="ui-li-desc">
+                            + '</h3><p class="ui-li-desc">'
                             + checkin.pts
                             + ' points</p></li>');
         });
