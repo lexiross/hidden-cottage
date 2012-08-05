55898164b708b75d061518ec2679956c083c51e8
Nikhil Goel <ngoel36@gmail.com>
2012-08-04 18:28:52 -0700

    add stream

---
+++ stream.php
@@ -0,0 +1,13 @@
+<?php
+
+$con = mysql_connect('localhost', 'root', 'crimson');
+mysql_select_db('hiddencottage');
+
+$userid=0; 
+$result = mysql_query("SELECT * from checkins where fb_user_id='$userid' order by timestamp DESC");
+
+while ($row = mysql_fetch_array($result))
+{
+	echo $row['c_id']; 
+}
+?>
