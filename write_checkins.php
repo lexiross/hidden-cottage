feca50b054b084c00a72064c3f7c34537a656409
Nikhil Goel <ngoel36@gmail.com>
2012-08-04 19:58:39 -0700

    how far page

--- write_checkins.php
+++ write_checkins.php
@@ -1,15 +1,25 @@
+<html>
 <?php
 
 $con = mysql_connect('localhost', 'root', 'crimson');
 mysql_select_db('hiddencottage');
 
-$userid=$_GET['userid']; 
-$useeid=$_GET['useeid']; 
-$pts=$_GET['pts']; 
 
+if ($_GET['userid'])
+{
+	$userid=$_GET['userid']; 
+	$useeid=$_GET['useeid']; 
+	$pts=$_GET['pts']; 
 
-$result = mysql_query("INSERT INTO checkins VALUES (0,$userid, $useeid, NOW(), $pts)");
-
-echo "checked-in!"
+	$result = mysql_query("INSERT INTO checkins VALUES (0,$userid, $useeid, NOW(), $pts)");
+	echo "checked-in!"
+}
+else
+{
+	?>
+	<img src="http://i.imgur.com/Mi3Mz.jpg">
+	<?php	
+}
 
 ?>
+</html>
